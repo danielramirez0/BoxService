368a75b554d3cb89a502f1d11c462c647f7a36c7
Daniel Ramirez <zoerunner@gmail.com>
2021-11-08 14:46:04 -0800

    [BUG/SOLUTION] MYSQL DB migration issue solved by manually specifying the datatype in the model

--- backendAPI/drf_jwt_backend/surveys/models.py
+++ backendAPI/drf_jwt_backend/surveys/models.py
@@ -6,16 +6,15 @@ from django.db import models
 class UserPlatform(models.Model):
     user = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
     platform = models.ForeignKey('games.Platform', on_delete=models.CASCADE, blank=True, null=True)
-    platform_name = models.CharField(max_length=50, blank=True, null=True)
 
 
 
 
 class UserPublisher(models.Model):
     user = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
-    publisher_name = models.CharField(max_length=50, blank=True, null=True)
+    publisher = models.ForeignKey('games.Publisher', on_delete=models.CASCADE, blank=True, null=True)
 
 
 class UserGenre(models.Model):
     user = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
-    genre_name = models.CharField(max_length=50, blank=True, null=True)
+    genre = models.ForeignKey('games.Genre', on_delete=models.CASCADE, blank=True, null=True)
